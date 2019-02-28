e63cee4045ae5563c1e7088844571b61e5be1955
Vasilyev <vasilyev508@gmail.com>
2019-02-28 17:17:40 +0200

    =

---
+++ js/main.js
@@ -0,0 +1,90 @@
+$(document).ready(function(){
+   slickSettings();
+   slickView();
+   langToggle();
+   currencyToggle();
+   loginPOPupView();
+   signUpPOPupView();
+
+});
+
+function slickView() {
+    $('.slick-next').html('&gt');
+    $('.slick-prev').html('&lt');
+    $('[id ^= slick-slide-control]').html('');
+}
+function slickSettings() {
+    $('.b__header--slider').slick({
+        dots : true,
+        infinite: true,
+        speed: 300,
+        autoplay : true,
+        fade: true,
+        cssEase: 'linear',
+        centerMode: true,
+    });
+}
+function langToggle() {
+    $('[class ^= b__topper--lang-]').click(function () {
+        $('[class ^= b__topper--lang-]' + '.active').removeClass('active');
+        $(this).addClass('active');
+    });
+}
+function currencyToggle(){
+    $('[class ^= b__topper--currency-]').click(function () {
+        $('[class ^= b__topper--currency-]' + '.active').removeClass('active');
+        $(this).addClass('active');
+    });
+}
+function createLogInPOPup(){
+    $('body').append("<div class='logInPOPup'></div>");
+    $('.logInPOPup').append("<div class='logInPOPup--wrapper'></div>");
+    $('.logInPOPup--wrapper').html('' +
+        '<label for="login"login>Login</label>'+
+        '<input type="text" name="login" placeholder="Login" maxlength="25" id="login">' +
+        '<label for="pass">Password</label>'+
+        '<input type="password" name="pass" placeholder="password"  id="pass" maxlength="25">'+
+        '<button class="sendLogin">LogIn</button>'
+    );
+    $('.logInPOPup--wrapper').click(function (event) {
+        event.stopImmediatePropagation();
+    });
+    $('.logInPOPup').click(function () {
+        destroyPOPupLogIn();
+    });
+    $('.sendLogin').click(function(){
+        alert('Login: ' + $('#login').val());
+    })
+}
+function loginPOPupView(){
+    $('.logIn').click(createLogInPOPup);
+}
+function destroyPOPupLogIn() {
+    $('.logInPOPup').remove();
+}
+function createSignUpPOPup(){
+    $('body').append("<div class='signUpPOPup'></div>");
+    $('.signUpPOPup').append("<div class='signUpPOPup--wrapper'></div>");
+    $('.signUpPOPup--wrapper').html('' +
+        '<label for="user"login>Login</label>'+
+        '<input type="text" name="login" placeholder="Login" maxlength="25" id="user">' +
+        '<label for="email">Password</label>'+
+        '<input type="email" name="email" placeholder="password"  id="email" maxlength="25">'+
+        '<button class="sendLogin">LogIn</button>'
+    );
+    $('.signUpPOPup--wrapper').click(function (event) {
+        event.stopImmediatePropagation();
+    });
+    $('.signUpPOPup').click(function () {
+        destroyPOPupSignUp();
+    });
+    $('.sendLogin').click(function(){
+        alert('Login: ' + $('#user').val() + ' '+ 'Email: '+  $('#email').val());
+    })
+}
+function signUpPOPupView(){
+    $('.signUp').click(createSignUpPOPup);
+}
+function destroyPOPupSignUp() {
+    $('.signUpPOPup').remove();
+}
